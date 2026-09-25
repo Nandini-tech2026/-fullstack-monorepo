7dea17aff5c05d93d4b2835cbc26afb8c0fcd781
Nandini DL <nandinilnandu2005@gmail.com>
2026-09-25 15:07:00 +0530

    Add files via upload

---
+++ server/src/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const healthRouter = require('../routes/health');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.use('/api/health', healthRouter);
+
+app.listen(PORT, () => {
+  console.log(Server running on http ://localhost:${PORT})
+});
+
+module.exports = app;
